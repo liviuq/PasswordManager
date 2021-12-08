959e6e7f8efd95596c70854371cc47dda1acb205
Liviu Andrei <petrache.andrei1@gmail.com>
2021-12-08 14:46:10 +0200

    Changed how client sends messages to server

--- source/client.c
+++ source/client.c
@@ -12,37 +12,35 @@
 
 int main(int argc, char *argv[])
 {
+	int32_t bytes_read, bytes_written;
+	(void)bytes_written;
 	int32_t exit_client = 0;
 	int32_t port;
-	int32_t sd;                                // descriptorul de socket
-	struct sockaddr_in server; // structura folosita pentru conectare
+	int32_t sd;				  //socket descriptor used to talk to the server 
+	struct sockaddr_in server; //struct for server ip, port, tcp option 
 
-	/* exista toate argumentele in linia de comanda? */
 	if (argc != 3)
 	{
-		printf("Sintaxa: %s <adresa_server> <port>\n", argv[0]);
-		return -1;
+		printf("[CLIENT]: %s ip_address port\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	/* stabilim portul */
+	//setting the port
 	port = atoi(argv[2]);
 
-	/* cream socketul */
+	//creating the socket
 	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		printf("%s on line %d\n", strerror(errno), __LINE__);
 		exit(EXIT_FAILURE);
 	}
 
-	/* umplem structura folosita pentru realizarea conexiunii cu serverul */
-	/* familia socket-ului */
+	//filling struct sockaddr with info
 	server.sin_family = AF_INET;
-	/* adresa IP a serverului */
 	server.sin_addr.s_addr = inet_addr(argv[1]);
-	/* portul de conectare */
 	server.sin_port = htons(port);
 
-	/* ne conectam la server */
+	//connecting to the server
 	if (connect(sd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1)
 	{
 		printf("%s on line %d\n", strerror(errno), __LINE__);
@@ -52,13 +50,17 @@ int main(int argc, char *argv[])
 	while (!exit_client)
 	{
 		// reading the number of bytes from the server
-		int32_t length_in = 0;
-		if (read(sd, &length_in, sizeof(length_in)) == -1)
+		int length_in = 0;
+		if ((bytes_read = read(sd, &length_in, sizeof(length_in))) == -1)
 		{
 			printf("%s on line %d\n", strerror(errno), __LINE__);
 			exit(EXIT_FAILURE);
 		}
-		printf("[CLIENT] Length of message is %d\n", length_in);
+		else if (bytes_read == 0)
+		{
+			printf("Goodbye server..\n");
+			return EXIT_SUCCESS;
+		}
 
 		// reading the whole message
 		char *incoming_message = malloc(sizeof(char) * length_in);
@@ -72,6 +74,7 @@ int main(int argc, char *argv[])
 
 		// displaying the message
 		printf("%s\n", incoming_message);
+		fflush(stdout);
 
 		// reading the input from the user
 		char *message = malloc(sizeof(char) * LOCAL_MESSAGE_LEN);
@@ -81,9 +84,10 @@ int main(int argc, char *argv[])
 			printf("%s on line %d\n", strerror(errno), __LINE__);
 			exit(EXIT_FAILURE);
 		}
+		// message[strlen(message)] = '\0';
 
 		// writing to the server the response's length
-		int32_t length_out = strlen(message);
+		int length_out = strlen(message);
 		if (write(sd, &length_out, sizeof(length_out)) == -1)
 		{
 			printf("%s on line %d\n", strerror(errno), __LINE__);
