99f4ff92a7edec52424244977a2e50e9f634cdf8
Petrache Liviu Andrei <petrache.andrei1@gmail.com>
2021-12-11 19:00:30 +0200

    Sync from laptop

--- source/xmlfunctions.c
+++ source/xmlfunctions.c
@@ -67,3 +67,61 @@ int32_t xmlCheckPassword(char *docname, char *password)
 	//no masterpass
 	return 0;
 }
+
+void xmlReplaceLoginField(char *docname, int32_t loginVal)
+{
+	xmlDocPtr document; // pointer to the document
+	xmlNodePtr current; // node pointer ot interact with individual nodes
+
+	// opening the document
+	// checkikng to see if the document was successfully parsed
+	if ((document = xmlParseFile(docname)) == NULL)
+	{
+		printf("Error on parsing the doc\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// retrieve document's root element
+	// check to see if document actually has something in it
+	if ((current = xmlDocGetRootElement(document)) == NULL)
+	{
+		printf("Error on parsing the doc\n");
+		xmlFreeDoc(document);
+		exit(EXIT_FAILURE);
+	}
+
+	// check to see that we opened the right type of document
+	if (xmlStrcmp(current->name, (const xmlChar *)"user"))
+	{
+		printf("Error on the doc\n");
+		xmlFreeDoc(document);
+		exit(EXIT_FAILURE);
+	}
+
+	current = current->children;
+	while (current != NULL)
+	{
+		if ((!xmlStrcmp(current->name, (const xmlChar *)"login")))
+		{
+			xmlChar *key = xmlNodeListGetString(document, current->children, 1);
+			if (!xmlStrcmp(key, (xmlChar *)password))
+			{
+				xmlFree(key);
+				xmlFreeDoc(document);
+				return 1; // password is verified
+			}
+			else
+			{
+				fflush(stdout);
+				printf("key:%s", key);
+				fflush(stdout);
+				xmlFree(key);
+				xmlFreeDoc(document);
+				return 0; // password is verified
+			}
+		}
+		current = current->next;
+	}
+
+	//no masterpass
+}
