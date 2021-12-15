fb9706917de9cf71501b94a48c9e94d9d41e6069
Liviu Andrei <petrache.andrei1@gmail.com>
2021-12-15 14:40:45 +0200

    Added xmlOpenUserFile function

--- include/xmlfunctions.h
+++ include/xmlfunctions.h
@@ -1,9 +1,10 @@
 #ifndef XMLFUNCTIONS_H_
 #define XMLFUNCTIONS_H_
 
-int32_t xmlCheckPassword(char *docname, char *password); //returns 1->password correct, 0->pass incorrect
-int32_t xmlCheckLogin(char *docname); //1->logged in, 0 ->logged off
+int32_t xmlCheckPassword(char *docname, char *password); // returns 1->password correct, 0->pass incorrect
+int32_t xmlCheckLogin(char *docname);                    // 1->logged in, 0 ->logged off
+void xmlOpenUserFile(char *docname, xmlDocPtr document, xmlNodePtr current);
 void xmlReplaceLoginField(char *docname, int32_t loginVal);
-void xmlCreateUser(const char* name, const char* password); //creates the basic xml schema for a user
+void xmlCreateUser(const char *name, const char *password); // creates the basic xml schema for a user
 
 #endif
